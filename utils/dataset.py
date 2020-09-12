4b7a47e13713edb83bc58c124fc99957e4556f49
Rahul Kumar Jain <toniqapps@gmail.com>
2020-09-12 22:18:36 +0530

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -8,3 +8,10 @@ def mnist():
   train = datasets.MNIST('./data', train=True, download=True, transform=train_transforms)
   test = datasets.MNIST('./data', train=False, download=True, transform=test_transforms)
   return train, test
+
+
+def cifar10():
+  train_transforms, test_transforms = cifar10_transforms()
+  train = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transforms)
+  test = datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transforms)
+  return train, test
